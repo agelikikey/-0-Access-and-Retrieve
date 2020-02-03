faf6fcee08870238fe71d0e5efd0b67c470e1076
agelikikey <agelikikey@gmail.com>
2020-02-03 18:38:33 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+//declaring the array
+var myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
+
+//1. print the item here
+// console.log(myArray);
+
+//2. change 'thursday'a value here to null
+myNewArray = [];
+myNewArray.push('sunday');
+myNewArray.push('monday');
+myNewArray.push('tuesday');
+myNewArray.push('wednesday');
+myNewArray.push(null);
+myNewArray.push('friday');
+myNewArray.push('saturday');
+
+
+//3. print the position of step 2
+console.log(myNewArray);
